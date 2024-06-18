df46176554e8556392b294681a5caa95a9241b4a
Ankit Panigrahi <panigrahi0702@gmail.com>
2024-06-18 21:40:14 +0530

    Final Working CLock with Switchable 12/24 hr button

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+var formatSwitchBtn = document.querySelector(".format-switch-btn");
+
+formatSwitchBtn.addEventListener("click", () => {
+    formatSwitchBtn.classList.toggle("active");
+
+    var formatValue = formatSwitchBtn.getAttribute("data-format");
+
+    if(formatValue == "12") {
+        formatSwitchBtn.setAttribute("data-format", "24");
+    }
+    else {
+        formatSwitchBtn.setAttribute("data-format", "12");
+    }
+});
+
 function clock() {
     var now = new Date();
     
@@ -12,7 +27,11 @@ function clock() {
     }
 
     //Setting the hours to 12 hour format
-    hours = hours > 12 ? hours % 12 : hours;
+    var formatValue = formatSwitchBtn.getAttribute("data-format");
+
+    if (formatValue == "12") { 
+            hours = hours > 12 ? hours % 12 : hours;
+    }
 
     if (hours < 10) {
         hours = '0' + hours;
@@ -32,3 +51,14 @@ function clock() {
     document.querySelector(".period").innerHTML = period;
 }
 var updateClock = setInterval(clock, 1000);
+
+var today = new Date();
+var dayNumber = today.getDate();
+var dayName = today.toLocaleString('default', { weekday: 'long' }); 
+var monthName = today.toLocaleString('default', { month: 'long' });
+var year = today.getFullYear();
+
+document.querySelector(".day-name").innerHTML = dayName;
+document.querySelector(".day-number").innerHTML = dayNumber;
+document.querySelector(".month-name").innerHTML = monthName;
+document.querySelector(".year").innerHTML = year;
