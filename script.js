3adf522a5cf8014f82dff730b086a27159b5fe8a
Ankit Panigrahi <panigrahi0702@gmail.com>
2024-06-18 18:35:31 +0530

    Clock working added Am to Pm functionality and 12/24 hr format

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function clock() {
-    const now = new Date();
+    var now = new Date();
     
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
     let period = 'AM';
     
     //Set period to PM if hours is greater than or equal to 12
@@ -11,10 +11,24 @@ function clock() {
         period = 'PM';
     }
 
+    //Setting the hours to 12 hour format
+    hours = hours > 12 ? hours % 12 : hours;
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
     document.querySelector(".hours").innerHTML = hours;
     document.querySelector(".minutes").innerHTML = minutes;
     document.querySelector(".seconds").innerHTML = seconds;
     document.querySelector(".period").innerHTML = period;
 }
-
-    var updateClock = setInterval(clock, 1000);
+var updateClock = setInterval(clock, 1000);
